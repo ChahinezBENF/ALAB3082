cf68fc7912724341eb818638d9d16be568e1e7b4
ChahinezBENF <Chahinez-benfriha27@outlook.com>
2025-02-27 00:42:02 -0500

    third commit

--- planet.js
+++ planet.js
@@ -84,7 +84,8 @@ switch (true) {
 
 
   //part 2//
-  console.log('****PART 2: Thinking Bigger****');
+  {
+    console.log('****PART 2: Thinking Bigger****');
   console.log('*****************************');
 
 week = 10;
@@ -101,3 +102,46 @@ console.log(`Space needed after  ${week} weeks is : ${spaceNeeded} m^2`);
 //for the new raduis i had to use the  avaScript function  that returns the square root
 let newRaduis = Math.sqrt(spaceNeeded / PI);
 console.log(` The radius of this expanded garden : ${newRaduis} m`);
+
+  }
+
+//part 3//
+console.log('****PART 3: Errors in Judgement****');
+console.log('*****************************');
+
+
+
+
+
+try {
+
+    const GardenCapacity = area / eachPlantSpace;
+    const plantNumberStart = 100;
+    let week = 0;
+    
+    if (plantNumberStart <=  GardenCapacity) {
+        const predictGrowth = plantNumberStart *(2**week);
+
+        const growPlant = (predictGrowth*100)/GardenCapacity;
+
+        switch (true) {
+        case growPlant > 80 : 
+        console.log (`Prune plant after week : ${week}.`);
+        break;
+        case growPlant >= 50 && growPlant <= 80:
+        console.log(`Monitor the plant after week : ${week}`);
+        break;
+        case growPlant < 50:
+        console.log(`plant more after week : ${week}. `);
+        break;
+         default:
+        console.log('Error ');
+        break;
+           }
+    } else {
+        throw "Error - the amount of space required to hold the originally provided number of plants exceeds the amount of space available.";
+    }
+} catch (error) {
+	console.log(error);
+}
+
